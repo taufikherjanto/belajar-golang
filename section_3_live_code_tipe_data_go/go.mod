6981296126b157a7367f4364c561ada7a1dae84e
Taufik Herjanto <taufik.herjanto@live.com>
2024-03-07 22:30:22 +0700

    Add section 3 live code tipe data Go

---
+++ section_3_live_code_tipe_data_go/go.mod
@@ -0,0 +1,3 @@
+module section_3_live_code_tipe_data_go
+
+go 1.22.0
---
+++ section_3_live_code_tipe_data_go/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Int
+	umur := 34
+
+	// String
+	nama := "Taufik"
+
+	// Float
+	suhu := 35.6
+
+	// Byte
+	var binaryData byte = 0b11010101
+
+	// Boolean
+	isMentor := false
+
+	// nil
+	var noValue interface{}
+
+	fmt.Println("Umur: ", umur)
+	fmt.Println("Nama: ", nama)
+	fmt.Println("Suhu: ", suhu)
+	fmt.Println("Binary data: ", binaryData)
+	fmt.Println("Is mentor: ", isMentor)
+	fmt.Println("No value: ", noValue)
+
+}
